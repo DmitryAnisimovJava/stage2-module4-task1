19722ae94dbd496b91a3161f6c1a46d2dcaf4ee0
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2024-07-26 16:52:03 +0300

    Add build() for Builder task

--- task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
+++ task1-builder-pattern/src/main/java/com/mjc/stage2/Employee.java
@@ -100,7 +100,7 @@ public class Employee {
             this.employee.carNumber = carNumber;
         }
 
-        public Employee getEmployee() {
+        public Employee build() {
             return this.employee;
         }
     }
