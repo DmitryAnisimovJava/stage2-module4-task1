edad33789c1515b14ddc780b085408e97e9d4dc4
DmitryAnisimovJava <DmitryAnisimovJava@users.noreply.github.com>
2024-07-26 16:59:09 +0300

    Correct instance.

--- task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
+++ task2-singleton-pattern/src/main/java/com/mjc/stage2/ThreadSafeSingleton.java
@@ -2,12 +2,12 @@ package com.mjc.stage2;
 
 public class ThreadSafeSingleton {
 
-    private static final ThreadSafeSingleton INSTANCE = new ThreadSafeSingleton();
+    private static ThreadSafeSingleton instance = new ThreadSafeSingleton();
 
     private ThreadSafeSingleton() {
     }
 
     public static ThreadSafeSingleton getInstance() {
-        return INSTANCE;
+        return instance;
     }
 }
